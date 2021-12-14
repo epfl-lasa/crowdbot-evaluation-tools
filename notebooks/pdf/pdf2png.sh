bfaf9216b045fb06c011d99faa820456b69e51c2
hibetterheyj <he-yujie@outlook.com>
2021-12-14 23:02:07 +0100

    update scripts to convert pdf to png with high quality

--- notebooks/pdf/pdf2png.sh
+++ notebooks/pdf/pdf2png.sh
@@ -1,7 +1,10 @@
 #!/bin/bash
 # sudo apt install imagemagick
-# https://unix.stackexchange.com/a/121297
+# 1. while-loop framework: https://unix.stackexchange.com/a/121297
+# 2. 'no images defined' error : https://askubuntu.com/a/1085339
+# 3. high-quality conversion: https://stackoverflow.com/a/13784772/7961693
+# convert -density 300 -trim test.pdf -quality 100 test.jpg
 find . -type f -name '*.pdf' -print0 |
     while IFS= read -r -d '' file; do
-        convert -verbose -density 500 -resize 800 "${file}" "${file%.*}.png"
+        convert -verbose -density 300 -quality 100 "${file}" "${file%.*}.png"
     done
