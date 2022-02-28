f2ac892cbb26ab5a57c29442c1c54fdad519ea93
hibetterheyj <he-yujie@outlook.com>
2022-02-28 15:14:58 +0100

    update significance annotation to boxplot in notebook

--- qolo/utils/notebook_util.py
+++ qolo/utils/notebook_util.py
@@ -228,6 +228,115 @@ def categorical_plot(
         ya.set_major_locator(MaxNLocator(integer=True))
 
 
+def barplot_annotate_brackets(
+    num1,
+    num2,
+    data,
+    center,
+    height,
+    line_y=None,
+    yerr=None,
+    dh=0.05,
+    barh=0.05,
+    fs=None,
+    maxasterix=None,
+):
+    """
+    Annotate barplot with p-values.
+    Original: https://stackoverflow.com/a/52333561/13954301
+    Modified by Yujie He
+
+    :param num1: number of left bar to put bracket over
+    :param num2: number of right bar to put bracket over
+    :param data: string to write or number for generating asterixes
+    :param center: centers of all bars (like plt.bar() input)
+    :param height: heights of all bars (like plt.bar() input)
+    :param yerr: yerrs of all bars (like plt.bar() input)
+    :param dh: height offset over bar / bar + yerr in axes coordinates (0 to 1)
+    :param barh: bar height in axes coordinates (0 to 1)
+    :param fs: font size
+    :param maxasterix: maximum number of asterixes to write (for very small p-values)
+    """
+    if type(data) is str:
+        text = data
+    else:
+        """
+        # * is p < 0.05
+        # ** is p < 0.005
+        # *** is p < 0.0005
+        # etc.
+        p = .05
+        text = ''
+
+
+        while data < p:
+            text += '*'
+            p /= 10.
+
+            if maxasterix and len(text) == maxasterix:
+                break
+        if len(text) == 0:
+            text = 'n. s.'
+        """
+        # p<0.1 --> *
+        # p<0.05 --> **
+        # p<0.01 --> ***
+        if 0.05 < data <= 0.1:
+            text = '*'
+        elif 0.01 < data <= 0.05:
+            text = '**'
+        elif data <= 0.01:
+            text = '***'
+        else:
+            text = ''
+            # text = 'n. s.'
+
+    if len(text) > 0:
+
+        lx, ly = center[num1], height[num1]
+        rx, ry = center[num2], height[num2]
+
+        if yerr:
+            ly += yerr[num1]
+            ry += yerr[num2]
+
+        ax_y0, ax_y1 = plt.gca().get_ylim()
+        dh *= ax_y1 - ax_y0
+        barh *= ax_y1 - ax_y0
+
+        if line_y is not None:
+            barx = [lx, lx, rx, rx]
+            bary = [line_y, line_y + barh, line_y + barh, line_y]
+            mid = ((lx + rx) / 2, line_y + barh)
+        else:
+            y = max(ly, ry) + dh
+
+            barx = [lx, lx, rx, rx]
+            bary = [y, y + barh, y + barh, y]
+            mid = ((lx + rx) / 2, y + barh)
+
+        plt.plot(barx, bary, c='black')
+
+        kwargs = dict(ha='center', va='bottom')
+        if fs is not None:
+            kwargs['fontsize'] = fs
+
+        plt.text(*mid, text, **kwargs)
+
+
+def main():
+    heights = [1.8, 2, 3]
+    bars = np.arange(len(heights))
+
+    plt.figure()
+    plt.bar(bars, heights, align='center')
+    plt.ylim(0, 5)
+    barplot_annotate_brackets(0, 1, 0.1, bars, heights)
+    barplot_annotate_brackets(1, 2, 0.001, bars, heights)
+    barplot_annotate_brackets(0, 2, 'p < 0.0075', bars, heights, dh=0.2)
+    plt.show()
+
+
 def import_eval_res(
     eval_dirs,
     crowd_metrics=None,
@@ -235,6 +344,7 @@ def import_eval_res(
     control_metrics=None,
     travel_path_thres=5.0,
 ):
+    """Load generated results as pd.DataFrame"""
     if not crowd_metrics:
         crowd_metrics = (
             'avg_crowd_density2_5',
